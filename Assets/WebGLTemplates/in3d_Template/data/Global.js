6ddc40f0bbda3e83213ace4dc12c81b3522cf45a
Alteracia <48139187+Alteracia@users.noreply.github.com>
2022-10-16 17:53:08 +0300

    Add template 2020 up

---
+++ Assets/WebGLTemplates/in3d_Template/data/Global.js
@@ -0,0 +1,8 @@
+var vtoFrame = document.getElementById("vto-frame");
+var vtoContainer = document.getElementById("vto-container");
+var container = document.querySelector("#unity-container");
+var canvas = document.querySelector("#unity-canvas");
+var vtoHideButton = document.getElementById("vto-hide-button");
+var canvasWrapper = document.getElementById("canvas-wrap");
+var gameInstance;
+var partner;
